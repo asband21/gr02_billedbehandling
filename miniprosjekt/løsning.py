64da51574bf53cb8546b3ab74739510f7694c75a
asbjørn <qw41ha@student.aau.dk>
2022-11-25 10:46:31 +0100

    100 linjer

--- miniprosjekt/løsning.py
+++ miniprosjekt/løsning.py
@@ -1,63 +1,22 @@
 import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
-import pdb
-
-deb = False
-def maske_prosent(hsv, neder, øver):
-    mask = cv.inRange(hsv, neder, øver)
-    #cv.imshow("mask",mask)
-    return mask.sum()/2560000
-
 
 def brak_type(img):
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    
-    # mark
-    tal = maske_prosent(hsv, np.array([23,150,150]) ,np.array([28,255,255]))
-    if 0.4 < tal:
-        #cv.imshow("mark"+str(tal),img)
-        #print("vand")
+    if 0.4 < cv.inRange(hsv, np.array([23,150,150]) ,np.array([28,255,255])).sum()/2560000:
         return 1
-
-    # vand
-    tal = maske_prosent(hsv, np.array([100,90,90]) ,np.array([140,255,255]))
-    if 0.4 < tal:
-        #cv.imshow("vand"+str(tal),img)
-        #print("vand")
+    if 0.4 < cv.inRange(hsv, np.array([100,90,90]) ,np.array([140,255,255])).sum()/2560000:
         return 2
-
-    # eng
-    tal = maske_prosent(hsv, np.array([35,90,90]) ,np.array([45,255,255]))
-    if 0.2 < tal:
-        #cv.imshow("eng"+str(tal),img)
-        #print("eng")
+    if 0.2 < cv.inRange(hsv, np.array([35,90,90]) ,np.array([45,255,255])).sum()/2560000:
         return 3
-
-    # skov
-    tal = maske_prosent(hsv, np.array([35,70,20]) ,np.array([65,255,90]))
-    if 0.2 < tal:
-        #cv.imshow("skov"+str(tal),img)
-        #print("skov")
+    if 0.2 < cv.inRange(hsv, np.array([35,70,20]) ,np.array([65,255,90])).sum()/2560000:
         return 4
-
-    # mine
-    tal = maske_prosent(hsv, np.array([0,0,0]) ,np.array([255,120,35]))
-    if 0.2 < tal:
-        #cv.imshow("mine"+str(tal),img)
-        #print("mine")
+    if 0.2 < cv.inRange(hsv, np.array([0,0,0]) ,np.array([255,120,35])).sum()/2560000:
         return 5
-
-    #sump
-    tal = maske_prosent(hsv, np.array([15,0,0]) ,np.array([30,250,250]))
+    tal = cv.inRange(hsv, np.array([15,0,0]) ,np.array([30,250,250])).sum()/2560000
     kant = cv.Canny(hsv[:,:,2],60,120).sum()
-    #print(tal)
-    #cv.imshow(str(kant) + " sump "+str(tal),img)
     if 0.65 < tal and kant > 200000:
-        #print(kant.sum())
-        #print("sump")
         return 6
-
     return 7
 
 def antal_kroner(img):
@@ -65,8 +24,6 @@ def antal_kroner(img):
     index = 0;
     max_sum = 0;
     teller = 0
-    if deb:
-        cv.imshow("felt",img)
     kroner = cv.imread("./king_domino_dataset/krone_master.png")
     for i in range(4):
         ud[i] = cv.matchTemplate(img,kroner,cv.TM_CCOEFF_NORMED)
@@ -75,12 +32,8 @@ def antal_kroner(img):
         if ud[i].sum() > max_sum:
             max_sum = ud[i].sum()
             index = i
-        if deb:
-            cv.imshow(str(i),ud[i])
     if(max_sum > 0):
         ret , markers = cv.connectedComponents(ud[index])
-        if deb:
-            cv.waitKey(0)
         return ret-1;
     return 0
 
@@ -92,15 +45,7 @@ def poing(img):
             li_img = img[(i*100):(i*100+100),(j*100):(j*100+100)]
             m_img[i,j,0] = brak_type(li_img)
             m_img[i,j,1] = antal_kroner(li_img)
-    for i in range(5):
-        for j in range(5):
-            ud[i,j,0] = m_img[i,j,0]*10
     print(f"---ping:{berging_poing(m_img)}---")
-    m_img = cv.cvtColor(m_img, cv.COLOR_HSV2BGR)
-    ud = cv.cvtColor(ud, cv.COLOR_HSV2BGR)
-    if deb:
-        cv.imshow('lille',ud)
-
 
 class cl_poing:
     def __init__(self, img, x, y):
@@ -112,11 +57,7 @@ class cl_poing:
         self.kor = 0 
 
     def berging_poing(self):
-        if(self.img[self.x,self.y,2] == 0):
-            return 0
-        if(self.col == 0):
-            return 0
-        if(self.img[self.x,self.y,0] != self.col):
+        if(self.img[self.x,self.y,2] == 0) or (self.col == 0) or (self.img[self.x,self.y,0] != self.col):
             return 0
         tutal = 0
         self.img[self.x,self.y,0] = 0
@@ -141,27 +82,12 @@ def berging_poing(img):
             tmp = cl_poing(img,i,j)
             t = tmp.berging_poing()
             tutal = tutal + t
-            if(t != 0):
-                print(f"fellt:{tmp.fel} kroner:{tmp.kor}")
     return tutal
-
-def sum_xy(array, start, stop, x_len, y_len):
-    tal = [0,0,0]
-    for x in range(start,x_len+start):
-        for y in range(stop,y_len+stop):
-            tal[0] = tal[0] + array[x,y][0]
-            tal[1] = tal[1] + array[x,y][1]
-            tal[2] = tal[2] + array[x,y][2]
-    for i in range(3):
-        tal[i] = tal[i]/(x_len*y_len)
-    return tal
 
 teller = np.random.randint(0,high=74);
 while(1):
-    sti = "king_domino_dataset/cropped_and_perspective_corrected_boards/"
-    img = cv.imread(sti + str(teller+1) + ".jpg")
+    img = cv.imread("king_domino_dataset/cropped_and_perspective_corrected_boards/" + str(teller+1) + ".jpg")
     cv.imshow("brat",img)
-
     k = cv.waitKey(5) & 0xFF
     if k == 110:
         teller = (teller + 1 ) % 74
@@ -172,4 +98,3 @@ while(1):
     if k == 99:
         poing(img)
 cv.destroyAllWindows()
-

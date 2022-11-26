318571af72b832b3d52527c18aa52a03e0dd25fb
asbjørn <qw41ha@student.aau.dk>
2022-11-26 23:09:59 +0100

    ranset ud

--- lecture/1./1-showPicture.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-# Open picture
-python_logo = cv2.imread("python.png", cv2.IMREAD_GRAYSCALE)
-
-# Display the picture
-cv2.imshow("Our window", python_logo)
-cv2.waitKey(0)
--- miniprosjekt/king_domino_dataset/cropped_and_perspective_corrected_boards/plot.p
+++
@@ -1,3 +0,0 @@
-set terminal png size 1500,1500 enhanced font "Helvetica,20"
-set output 'h_mono_plot.png'
-plot 'h_mono.csv'
